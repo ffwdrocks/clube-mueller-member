35838d629ded6118535f058a0291676a7bdc80fb
victorscher <victorscher92@gmail.com>
2021-07-29 14:48:35 -0300

    feat: add dynamic invited-list

---
+++ js/invited-list.js
@@ -0,0 +1,15 @@
+const invitedListBtn = document.getElementById('invited-list-btn');
+const invitedBox = document.getElementById('invited-box')
+let isOpen = false;
+
+invitedListBtn.addEventListener('click', () => {
+    if(!isOpen){
+        invitedListBtn.classList.add('open')
+        invitedBox.classList.add('open')
+        isOpen = !isOpen
+    }else{
+        invitedListBtn.classList.remove('open')
+        invitedBox.classList.remove('open')
+        isOpen = !isOpen
+    }
+})
